e087a2cde33b931e8f4c8fab24d515d01b1607db
etomknudsen <40541430+etomknudsen@users.noreply.github.com>
2019-12-15 10:00:10 -0800

    Update pay.py

--- payout/pay.py
+++ payout/pay.py
@@ -14,25 +14,26 @@ print('Cycle;Address;RewardShare;Reward;PayOut;Fee')
 
 # Go through cycles one by one
 for i in range(cycle_from, cycle_to):
-        # Get total baker rewards for cycle
-        url_api = '{}/tables/income?address={}&cycle={}&columns=total_income,total_lost,balance,delegated'.format(api_server, baker_pkh, i)
-        response = urllib.urlopen(url_api)
-        data = json.loads(response.read().decode("utf-8"))
-        total_income =  float(data[0][0])
-        total_lost = float(data[0][1])
-        baker_balance = float(data[0][2])
-        baker_delegated = float(data[0][3])
-        total_rewards = total_income - total_lost
-        # Get delegates for cycle
-        url_api = '{}/tables/snapshot?delegate={}&cycle={}&is_selected=1&columns=address,balance,cycle'.format(api_server, baker_pkh, i-7)
-        response = urllib.urlopen(url_api)
-        data = json.loads(response.read().decode("utf-8"))
-        for delegate in data:
-                delegate_pkh = delegate[0]
-                void_fee = 1 - int(delegate_pkh == baker_pkh)
-                delegate_balance = delegate[1]
-                delegate_rewardshare = delegate_balance/(baker_delegated+baker_balance)
-                delegate_rewards = total_rewards * delegate_rewardshare
-                delegate_fee = delegate_rewards * (100 - fee_percent * void_fee)/100
-                delegate_payout = delegate_rewards - delegate_fee
-                print '{};{};{}%;{};{};{}'.format(i, delegate_pkh, delegate_rewardshare * 100, delegate_rewards, delegate_fee, delegate_payout)
+	# Get total baker rewards for cycle
+	url_api = '{}/tables/income?address={}&cycle={}&columns=total_income,total_lost,balance,delegated'.format(api_server, baker_pkh, i)
+	response = urllib.urlopen(url_api)
+	data = json.loads(response.read().decode("utf-8"))
+	total_income =  float(data[0][0])
+	total_lost = float(data[0][1])
+	baker_balance = float(data[0][2])
+	baker_delegated = float(data[0][3])
+	total_rewards = total_income - total_lost 
+	# Get delegates for cycle when rewards were earned
+	url_api = '{}/tables/snapshot?delegate={}&cycle={}&is_selected=1&columns=address,balance,cycle'.format(api_server, baker_pkh, i-7)
+	response = urllib.urlopen(url_api)
+	data = json.loads(response.read().decode("utf-8"))
+	for delegate in data: 
+		delegate_pkh = delegate[0]
+		void_fee = 1 - int(delegate_pkh == baker_pkh)
+		delegate_balance = delegate[1]
+		delegate_rewardshare = delegate_balance/(baker_delegated+baker_balance)  
+		delegate_rewards = total_rewards * delegate_rewardshare
+		delegate_fee = delegate_rewards * (100 - fee_percent * void_fee)/100
+		delegate_payout = delegate_rewards - delegate_fee  
+		print '{};{};{}%;{};{};{}'.format(i, delegate_pkh, delegate_rewardshare * 100, delegate_rewards, delegate_fee, delegate_payout)
+
