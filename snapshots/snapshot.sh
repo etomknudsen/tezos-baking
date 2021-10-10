509a6eebed7c65a6a5a36eb0d5d5b5fe8c349085
etomknudsen <40541430+etomknudsen@users.noreply.github.com>
2021-10-10 09:56:20 -0700

    Update snapshot.sh

--- snapshots/snapshot.sh
+++ snapshots/snapshot.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+#set -eux
+
 tezos/tezos-node snapshot export .snapshots/$(curl -s 127.0.0.1:8732/chains/main/blocks/head/header | jq '.hash' | xargs).rolling --rolling --block $(curl -s 127.0.0.1:8732/chains/main/blocks/head/header | jq '.hash' | xargs)
