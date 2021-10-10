cb041c43c22fae4694691beab503ab31071afd1e
etomknudsen <40541430+etomknudsen@users.noreply.github.com>
2021-10-10 09:54:55 -0700

    Create snapshot.sh

---
+++ snapshots/snapshot.sh
@@ -0,0 +1,1 @@
+tezos/tezos-node snapshot export .snapshots/$(curl -s 127.0.0.1:8732/chains/main/blocks/head/header | jq '.hash' | xargs).rolling --rolling --block $(curl -s 127.0.0.1:8732/chains/main/blocks/head/header | jq '.hash' | xargs)
